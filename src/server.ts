df0750cff92995f99eb68771880bf4002a4f44e2
Arthur Vinicius <thuur.vss@gmail.com>
2022-06-05 13:57:59 -0300

    Configurando ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import express, { Request, Response } from "express";
 
-import { createCourse } from "./routes";
-
 const app = express();
 
 app.listen(3333, () =>
   console.log("Servidor online no likn: http://localhost:3333")
 );
 
-app.get("/", (request: Request, response: Response) => {
-  return response.json({
-    status: "On-line",
-  });
-});
-
-app.get("/createCouse", createCourse);
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+    message: "Servidor On-line!"
+  })
+})
