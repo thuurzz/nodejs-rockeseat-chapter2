2f37918b3f68b0e24a37123f1ce5b269f6a692be
Arthur Vinicius <thuur.vss@gmail.com>
2022-06-05 14:02:54 -0300

    Debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json)
+
 app.listen(3333, () =>
   console.log("Servidor online no likn: http://localhost:3333")
 );
@@ -9,5 +11,12 @@ app.listen(3333, () =>
 app.get("/", (req: Request, res: Response) => {
   return res.json({
     message: "Servidor On-line!"
+  })
+})
+
+app.post("/courses", (req: Request, res: Response) => {
+  const { name } = req.body;
+  return res.json({
+    name
   })
 })
