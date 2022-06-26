ea41f87ab2ec03dfa3dc1becb4dd856d2890e7de
Arthur Vinicius <thuur.vss@gmail.com>
2022-06-25 23:58:39 -0300

    Correção imports

--- src/modules/cars/useCases/createCatogory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCatogory/CreateCategoryController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+ import { Request, Response } from 'express';
 import { CreateCategoryUseCase } from './CreateCategotyUseCase'
 
 class CreateCategoryController {
